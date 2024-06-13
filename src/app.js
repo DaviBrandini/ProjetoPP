68381973b9c3c01d493915985fde968ec21ab639
DAVI <davishillebrand8@gmail.com>
2024-06-13 10:37:58 -0300

    api pronta

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@
 const express = require('express');
 // Importar o pacote dotenv, gerenciador de variáveis de ambiente
 const dotenv = require( 'dotenv').config();
+const taskRouter = require('./routes/taskRouter');
+const cors = require('cors');
 
 // Instanciar o express na variável app
 const app = express();
 // Setar a porta do servidor, a parir do arquivo .env ou assumir 3005
 app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(cors());
+app.use('/api', taskRouter);
 
 module.exports = app;
